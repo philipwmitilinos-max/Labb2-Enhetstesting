1f5c3bbea674776c8f2420465b0c1ea2bdf6b08b
philipwmitilinos-max <philipwmitilinos@gmail.com>
2026-02-04 16:20:15 +0100

    Har skapat labben. Har gjort AccountComponentTest och AccountComponent.

--- src/main/java/se/iths/philip/bankkonto/component/AccountComponent.java
+++ src/main/java/se/iths/philip/bankkonto/component/AccountComponent.java
@@ -4,4 +4,21 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AccountComponent {
+
+    private int saldo = 0;
+
+    //Deposit för att sätta in på saldo
+    public void deposit(int amount) {
+        saldo += amount;
+    }
+
+    //Withdraw för att ta ut ifrån saldo
+    public void withdraw(int amount) {
+        saldo -= amount;
+    }
+
+    //För att kunna hämta saldo
+    public int getSaldo() {
+        return saldo;
+    }
 }
--- src/test/java/se/iths/philip/bankkonto/component/AccountComponentTest.java
+++ src/test/java/se/iths/philip/bankkonto/component/AccountComponentTest.java
@@ -3,6 +3,8 @@ package se.iths.philip.bankkonto.component;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
 public class AccountComponentTest {
     private AccountComponent accountComponent;
 
@@ -19,14 +21,29 @@ public class AccountComponentTest {
      * Att sätta in och ta ut ifrån saldo i samma metod 0 + x - y = xy
      */
 
+
+    @Test
+    public void testAccountIsZero() {
+        //Testar att saldot är noll
+        assertEquals("Saldo är 0 när kontot skapas", 0,
+                accountComponent.getSaldo());
+    }
+
     @Test
     public void testAddToAccount() {
         //Testar att det kommer in på saldot
+        accountComponent.deposit(1000); //act
+        assertEquals("Saldo ska öka efter insättning", 1000,
+                accountComponent.getSaldo()); //assert
     }
 
     @Test
     public void testTakeFromAccount() {
         //Testat att ta ut ifrån saldot
+        accountComponent.deposit(1000);
+        accountComponent.withdraw(450); //act
+        assertEquals("Saldo ska minska efter uttag", 550,
+                accountComponent.getSaldo()); //assert
     }
 
     @Test
@@ -36,6 +53,6 @@ public class AccountComponentTest {
 
     @Test
     public void testPutInAndOutFromAccount() {
-        //Testar att sätta in och att ta ut ifrån saldot
+        //Testar att sätta in och att ta ut ifrån saldot. (Jag behöver inte göra det här testet)
     }
 }
